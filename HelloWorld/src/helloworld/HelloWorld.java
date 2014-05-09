1c702bd8a1ce7d588452872bb25c899bf948be12
fansy1990 <912330679@qq.com>
2014-05-09 17:40:27 +0800

    e

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -8,7 +8,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
-		System.out.println("second edited");
 	}
 
 }
